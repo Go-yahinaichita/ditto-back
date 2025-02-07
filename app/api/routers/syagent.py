5d474e0bda79e6bd25d2f9b2919267aba0d61516
Haaaaya <hayatosetoya@gmail.com>
2025-02-07 11:49:41 +0900

    fix: fixed api

--- app/api/routers/syagent.py
+++ app/api/routers/syagent.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
+from fastapi.responses import StreamingResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.crud import syagent as syagent_crud
 from app.db.session import get_db
 from app.schemas import syagent as syagent_schema
 from app.schemas import utils as utils_schema
@@ -9,9 +11,11 @@ router = APIRouter(prefix="/agents", tags=["agents"])
 
 
 # /agents/{user_id} に対するエンドポイントを定義
-@router.get("/{user_id}", response_model=syagent_schema.CurrentProfile)
-async def get_conversations(user_id: int, db: AsyncSession = Depends(get_db)):
-    pass
+@router.get("/{user_id}", response_model=list[syagent_schema.OutputConversation])
+async def get_conversations(
+    user_id: int, db: AsyncSession = Depends(get_db)
+) -> list[syagent_schema.OutputConversation]:
+    return await syagent_crud.read_conversations(db, user_id)
 
 
 @router.post("/{user_id}", response_model=syagent_schema.OutputConversation)
@@ -19,8 +23,8 @@ async def post_conversation(
     user_id: int,
     current_profile: syagent_schema.CurrentProfile,
     db: AsyncSession = Depends(get_db),
-):
-    pass
+) -> syagent_schema.OutputConversation:
+    return await syagent_crud.create_conversation(db, user_id, current_profile)
 
 
 # /agents/conversations/{conversation_id} に対するエンドポイントを定義
@@ -29,7 +33,7 @@ async def post_conversation(
     response_model=list[syagent_schema.OutputMessage],
 )
 async def get_messages(conversation_id: int, db: AsyncSession = Depends(get_db)):
-    pass
+    return await syagent_crud.read_messages(db, conversation_id)
 
 
 @router.post(
@@ -39,8 +43,16 @@ async def post_message(
     conversation_id: int,
     input_message: syagent_schema.InputMessage,
     db: AsyncSession = Depends(get_db),
-):
-    pass
+) -> StreamingResponse:
+    output_message = await syagent_crud.create_message(
+        db, conversation_id, input_message
+    )
+
+    async def stream_messages():
+        for message in output_message:
+            yield message
+
+    return StreamingResponse(stream_messages(), media_type="text/event-stream")
 
 
 @router.delete(
@@ -50,4 +62,4 @@ async def delete_conversation(
     conversation_id: int,
     db: AsyncSession = Depends(get_db),
 ):
-    pass
+    return await syagent_crud.delete_conversation(db, conversation_id)
--- app/crud/syagent.py
+++ app/crud/syagent.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_google_vertexai import ChatVertexAI
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -14,6 +15,9 @@ llm = ChatVertexAI(model="gemini-1.5-flash-002")
 async def read_conversations(
     db: AsyncSession, user_id: int
 ) -> list[syagent_schema.OutputConversation]:
+    """
+    ユーザが持つ会話のリストを取得する。
+    """
     result = await db.execute(
         select(syagent_model.Conversation).filter_by(user_id=user_id)
     )
@@ -29,18 +33,66 @@ async def read_conversations(
 async def create_conversation(
     db: AsyncSession, user_id: int, current_profile: syagent_schema.CurrentProfile
 ) -> syagent_schema.OutputConversation:
-    # future_avatar作成
-    sim_wf = syagent_service.SimulationWorkflow(llm, current_profile)
-    future_profile = syagent_model.FutureAvatar(**sim_wf.generate().model_dump())
-    db.add(future_profile)
+    """
+    ユーザの情報から、将来の自己像を生成し、会話を開始する。
+    """
+    # CurrentProfile 作成
+    current_profile_model = syagent_model.CurrentProfile(
+        user_id=user_id, status=current_profile.status
+    )
+    db.add(current_profile_model)
     await db.flush()
-    # conversation作成
+
+    # CurrentSkill, FutureGoal 作成
+    # スキルの登録
+    current_skills = [
+        syagent_model.CurrentSkill(
+            current_profile_id=current_profile_model.id, skill=skill
+        )
+        for skill in current_profile.skills
+    ]
+    for skill in current_skills:
+        db.add(skill)
+
+    # 目標の登録
+    future_goals = [
+        syagent_model.FutureGoal(current_profile_id=current_profile_model.id, goal=goal)
+        for goal in current_profile.future_goals
+    ]
+    for goal in future_goals:
+        db.add(goal)
+
+    # FutureProfile 作成
+    # シミュレーションワークフローにより将来の自己像を生成
+    sim_wf = syagent_service.SimulationWorkflow(llm, current_profile)
+    generated_future_profile = sim_wf.generate()
+    future_profile = syagent_model.FutureProfile(
+        user_id=user_id,
+        current_profile_id=current_profile_model.id,
+        status=generated_future_profile.status,
+        time_frame=generated_future_profile.time_frame,
+    )
+    db.add(future_profile)
+    await db.flush()  # future_profile.id が取得可能になる
+
+    # FutureSkill 作成
+    future_skills = [
+        syagent_model.FutureSkill(future_profile_id=future_profile.id, skill=skill)
+        for skill in generated_future_profile.skills
+    ]
+    for skill in future_skills:
+        db.add(skill)
+
+    # Conversation 作成
     conversation = syagent_model.Conversation(
         user_id=user_id,
-        future_avatar_id=future_profile.id,
+        future_profile_id=future_profile.id,
         title="Conversation",
     )
     db.add(conversation)
+    await db.flush()
+
+    # Pydantic スキーマに変換して返却
     return syagent_schema.OutputConversation.model_validate(conversation)
 
 
@@ -60,16 +112,107 @@ async def read_messages(
 
 async def create_message(
     db: AsyncSession, conversation_id: int, input_message: syagent_schema.InputMessage
-) -> syagent_schema.OutputMessage:
+) -> list[str]:
+    # conversation取得
+    conversation_result = await db.execute(
+        select(syagent_model.Conversation).filter_by(id=conversation_id)
+    )
+    conversation = conversation_result.scalar()
+    if not conversation:
+        raise HTTPException(status_code=404, detail="Conversation not found")
+
+    # messages取得
+    messages_result = await db.execute(
+        select(syagent_model.Message).filter_by(conversation_id=conversation_id)
+    )
+    messages = messages_result.scalars().all()
+    if not messages:
+        messages = []
+
+    # future_profile取得
+    future_profile_result = await db.execute(
+        select(syagent_model.FutureProfile).filter_by(id=conversation.future_profile_id)
+    )
+    future_profile = future_profile_result.scalar()
+    if not future_profile:
+        raise HTTPException(status_code=404, detail="Futureprofile not found")
+    # future_skills取得
+    future_skills_result = await db.execute(
+        select(syagent_model.FutureSkill).filter_by(future_profile_id=future_profile.id)
+    )
+    future_skills = future_skills_result.scalars().all()
+    if not future_skills:
+        future_skills = []
+
+    # current_profile取得
+    current_profile_result = await db.execute(
+        select(syagent_model.CurrentProfile).filter_by(
+            id=future_profile.current_profile_id
+        )
+    )
+    current_profile = current_profile_result.scalar()
+    if not current_profile:
+        raise HTTPException(status_code=404, detail="CurrentProfile not found")
+    # current_skills取得
+    current_skills_result = await db.execute(
+        select(syagent_model.CurrentSkill).filter_by(
+            current_profile_id=current_profile.id
+        )
+    )
+    current_skills = current_skills_result.scalars().all()
+    if not current_skills:
+        current_skills = []
+    # future_goals取得
+    future_goals_result = await db.execute(
+        select(syagent_model.FutureGoal).filter_by(
+            current_profile_id=current_profile.id
+        )
+    )
+    future_goals = future_goals_result.scalars().all()
+    if not future_goals:
+        future_goals = []
+
+    # chat_workflow
+    chat_wf = syagent_service.ChatWorkflow(
+        llm,
+        syagent_schema.CurrentProfile(
+            age=20,  ###Placeholder
+            status=current_profile.status,
+            skills=[skill.skill for skill in current_skills] if current_skills else [],
+            values="",  ###Placeholder
+            restrictions="",  ###Placeholder
+            future_goals=[goal.goal for goal in future_goals] if future_goals else [],
+            extra="",  ###Placeholder
+        ),
+        syagent_schema.FutureProfile(
+            status=future_profile.status,
+            time_frame=future_profile.time_frame,
+            skills=[skill.skill for skill in future_skills] if future_skills else [],
+            summary="",  ###Placeholder
+        ),
+    )
+    history = []
+    if messages:
+        for message in messages:
+            if message.role == "user":
+                history.append(HumanMessage(content=message.message, id=message.id))
+            elif message.role == "agent":
+                history.append(AIMessage(content=message.message, id=message.id))
+    user_input = input_message.message
+    st_message = ""
+    streaming_messages: list[str] = []
+    async for chunk in chat_wf.process_input(history, user_input):
+        st_message += str(chunk)
+        streaming_messages.append(str(chunk))
     message = syagent_model.Message(
         conversation_id=conversation_id, role="user", **input_message.model_dump()
     )
     output_message = syagent_model.Message(
-        conversation_id=conversation_id, role="agent", message="Hello!"
+        conversation_id=conversation_id, role="agent", message=st_message
     )
     db.add(message)
     db.add(output_message)
-    return syagent_schema.OutputMessage.model_validate(output_message)
+    return streaming_messages
 
 
 async def delete_conversation(db: AsyncSession, conversation_id: int):
