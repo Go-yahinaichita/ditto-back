f047a734a759fc3b81eb8e16eb1da002d31aa49d
harutin <godaharuto@gmail.com>
2025-02-10 09:45:40 +0000

    fix: timezone

--- app/api/routers/syagent.py
+++ app/api/routers/syagent.py
@@ -7,7 +7,6 @@ from app.db.session import get_db
 from app.schemas import syagent as syagent_schema
 from app.schemas import utils as utils_schema
 
-
 router = APIRouter(prefix="/agents", tags=["agents"])
 
 
